101e8b7657fb21b38d5b0da61230ce00ba50e3fa
Dragory <2606411+Dragory@users.noreply.github.com>
2026-07-31 19:14:46 +0000

    refactor: new dockerfile + compose files

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+case "$1" in
+    migrate) exec pnpm run --silent run:migrate;;
+    bot) exec pnpm run --silent run:bot;;
+    api) exec pnpm run --silent run:api;;
+    dashboard) exec pnpm run --silent run:dashboard;;
+    *)
+        echo "Unknown command: $1"
+        exit 1
+        ;;
+esac
