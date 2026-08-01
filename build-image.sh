dbaa22a332b1b3214b9ead1bf11479a6c61123a6
Dragory <2606411+Dragory@users.noreply.github.com>
2026-08-01 08:38:54 +0000

    chore: add tag param to build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,7 @@
+TAG=${1:-latest}
+
 docker build \
-  -t dragory/zeppelin \
+  -t "dragory/zeppelin:$TAG" \
   --build-arg COMMIT_HASH=$(git rev-parse HEAD) \
   --build-arg BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
   .
